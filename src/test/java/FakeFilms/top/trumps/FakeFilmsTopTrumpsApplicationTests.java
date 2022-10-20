6a453570d72feb50edfdb1996def1e4edb0f1bcd
Ryan Garrett(Adder12) <ryan.garrett011@gmail.com>
2022-10-20 12:19:16 +0100

    troubleshooting failed build

--- src/test/java/FakeFilms/top/trumps/FakeFilmsTopTrumpsApplicationTests.java
+++ src/test/java/FakeFilms/top/trumps/FakeFilmsTopTrumpsApplicationTests.java
@@ -3,11 +3,11 @@ package FakeFilms.top.trumps;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-//@SpringBootTest
-//class FakeFilmsTopTrumpsApplicationTests {
-//
+@SpringBootTest
+class FakeFilmsTopTrumpsApplicationTests {
+
 //	@Test
 //	void contextLoads() {
 //	}
-//
-//}
+
+}
