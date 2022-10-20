d380727cb623e8537e629168dc3e5ffbf6945ee4
Ryan Garrett(Adder12) <ryan.garrett011@gmail.com>
2022-10-20 15:17:51 +0100

    Working get mapping method

---
+++ src/main/java/Fake/Films/top/trumps/Actor.java
@@ -0,0 +1,55 @@
+package Fake.Films.top.trumps;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "actor")
+public class Actor {
+    //Attributes--------------------------------------------------------
+
+    @Id
+    @Column(name = "actor_id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    int actorId;
+
+
+    @Column(name = "first_name")
+    String firstName;
+
+    @Column(name = "last_name")
+    String lastName;
+
+    //Constructors----------------------------------------------------------------------------------------
+
+    public Actor(String firstName, String lastName) {
+
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    public Actor() {
+
+    }
+
+    //Methods-----------------------------------------------------------------------------------------------
+    public int getActorId() {
+        return this.actorId;
+    }
+
+    public String getFirstName() {
+        return this.firstName;
+    }
+
+    public String getLastName() {
+        return this.lastName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+}
--- src/main/java/Fake/Films/top/trumps/FakeFilmsTopTrumpsApplication.java
+++ src/main/java/Fake/Films/top/trumps/FakeFilmsTopTrumpsApplication.java
@@ -1,13 +1,36 @@
 package Fake.Films.top.trumps;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.*;
 
 @SpringBootApplication
+@RestController
+@RequestMapping("/home")
+@CrossOrigin
 public class FakeFilmsTopTrumpsApplication {
+    @Autowired
+    private ActorRepository actorRepo;
 
-	public static void main(String[] args) {
-		SpringApplication.run(FakeFilmsTopTrumpsApplication.class, args);
-	}
+    public FakeFilmsTopTrumpsApplication(ActorRepository actorRepo) {
+        this.actorRepo = actorRepo;
+    }
 
+    public static void main(String[] args) {
+
+        SpringApplication.run(FakeFilmsTopTrumpsApplication.class, args);
+
+
+    }
+
+    @GetMapping("/allActors")
+    public @ResponseBody
+    Iterable<Actor> getAllActors() {
+        return actorRepo.findAll();
+    }
+//@PostMapping
+	
+//@PutMapping
+//	@DeleteMapping
 }
