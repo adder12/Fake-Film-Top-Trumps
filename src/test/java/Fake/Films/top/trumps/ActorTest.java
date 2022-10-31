905eea0f7cb82df09bc274e7c344fa0a56e9dcb7
Ryan Garrett(Adder12) <ryan.garrett011@gmail.com>
2022-10-31 15:49:02 +0000

    Removed code smells

--- src/test/java/Fake/Films/top/trumps/ActorTest.java
+++ src/test/java/Fake/Films/top/trumps/ActorTest.java
@@ -4,25 +4,25 @@ package Fake.Films.top.trumps;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-public class ActorTest {
+class ActorTest {
     Actor testActor = new Actor();
     Actor testActorTwo = new Actor("test", "test");
 
     @Test
-    public void testGetActorId() {
+    void testGetActorId() {
 
         Assertions.assertEquals(0, testActorTwo.getActorId(), "The actor ID is incorrect");
 
     }
 
     @Test
-    public void testgetFirstName() {
+    void testgetFirstName() {
         testActor.setFirstName("test");
         Assertions.assertEquals("test", testActor.getFirstName(), "The first name was incorrect");
     }
 
     @Test
-    public void testgetLastName() {
+    void testgetLastName() {
         testActor.setLastName("test");
         Assertions.assertEquals("test", testActor.getLastName(), "The last name was incorrect");
     }
--- src/test/java/Fake/Films/top/trumps/FilmTest.java
+++ src/test/java/Fake/Films/top/trumps/FilmTest.java
@@ -3,25 +3,25 @@ package Fake.Films.top.trumps;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-public class FilmTest {
+class FilmTest {
 
     Film testFilm = new Film();
     Film newFilm = new Film("test", "test", 2000, 20.4, 90, 10.53, "PG");
 
 
     @Test
-    public void testGetFilmId() {
+    void testGetFilmId() {
         Assertions.assertEquals(0, testFilm.getFilmId(), "The id was incorrect");
     }
 
     @Test
-    public void testTitle() {
+    void testTitle() {
         testFilm.setTitle("test");
         Assertions.assertEquals("test", testFilm.getTitle(), "The title is incorrect");
     }
 
     @Test
-    public void testDescription() {
+    void testDescription() {
         testFilm.setDescription("test");
         Assertions.assertEquals("test", testFilm.getDescription(), "The description was incorrect");
     }
@@ -33,135 +33,135 @@ public class FilmTest {
     }
 
     @Test
-    public void testRentalRate() {
+    void testRentalRate() {
         testFilm.setRentalRate(10.54);
         Assertions.assertEquals(10.54, testFilm.getRentalRate(), "The rental rate was incorrect");
     }
 
     @Test
-    public void testLength() {
+    void testLength() {
         testFilm.setLength(200);
         Assertions.assertEquals(200, testFilm.getLength(), "The length was incorrect");
     }
 
     @Test
-    public void testReplacementCost() {
+    void testReplacementCost() {
         testFilm.setReplacementCost(21.43);
         Assertions.assertEquals(21.43, testFilm.getReplacementCost(), "The replacement cost was incorrect");
     }
 
     @Test
-    public void testRating() {
+    void testRating() {
         testFilm.setRating("G");
         Assertions.assertEquals("G", testFilm.getRating(), "The rating was incorrect");
     }
 
     @Test
-    public void testGetIntRatingG() {
+    void testGetIntRatingG() {
         testFilm.setRating("G");
         Assertions.assertEquals(1, testFilm.getIntRating(), "The get Int rating method failed on G");
     }
 
     @Test
-    public void testGetIntRatingPG() {
+    void testGetIntRatingPG() {
         testFilm.setRating("PG");
         Assertions.assertEquals(2, testFilm.getIntRating(), "The get Int rating method failed on PG");
     }
 
     @Test
-    public void testGetIntRatingPG13() {
+    void testGetIntRatingPG13() {
         testFilm.setRating("PG-13");
         Assertions.assertEquals(3, testFilm.getIntRating(), "The get int rating method failed on PG-13");
     }
 
     @Test
-    public void testGetIntRatingNC17() {
+    void testGetIntRatingNC17() {
         testFilm.setRating("NC-17");
         Assertions.assertEquals(4, testFilm.getIntRating(), "The get int rating method failed on NC-17");
     }
 
     @Test
-    public void testGetIntRatingR() {
+    void testGetIntRatingR() {
         testFilm.setRating("R");
         Assertions.assertEquals(5, testFilm.getIntRating(), "The get int rating method failed on R");
     }
 
     @Test
-    public void testGetIntRatingDEF() {
+    void testGetIntRatingDEF() {
         testFilm.setRating("test");
         Assertions.assertEquals(0, testFilm.getIntRating(), "The get int rating method failed on the default case");
     }
 
     @Test
-    public void testCompareLengthLess() {
+    void testCompareLengthLess() {
         testFilm.setLength(89);
         Assertions.assertEquals(0, testFilm.compareLength(newFilm), "The length comparison failed on shorter");
     }
 
     @Test
-    public void testCompareLengthSame() {
+    void testCompareLengthSame() {
         testFilm.setLength(90);
         Assertions.assertEquals(1, testFilm.compareLength(newFilm), "The length comparison failed on equal");
     }
 
     @Test
-    public void testCompareLengthMore() {
+    void testCompareLengthMore() {
         testFilm.setLength(91);
         Assertions.assertEquals(2, testFilm.compareLength(newFilm), "The length comparison failed on longer");
     }
 
 
     @Test
-    public void testCompareRentalRateLess() {
+    void testCompareRentalRateLess() {
         testFilm.setRentalRate(20.39);
         Assertions.assertEquals(0, testFilm.compareRentalRate(newFilm), "The rental rate comparison failed on lesser");
     }
 
     @Test
-    public void testCompareRentalRateSame() {
+    void testCompareRentalRateSame() {
         testFilm.setRentalRate(20.4);
         Assertions.assertEquals(1, testFilm.compareRentalRate(newFilm), "The rental rate comparison failed on same");
     }
 
     @Test
-    public void testCompareRentalRateMore() {
+    void testCompareRentalRateMore() {
         testFilm.setRentalRate(20.41);
         Assertions.assertEquals(2, testFilm.compareRentalRate(newFilm), "The rental rate comparison failed om more");
     }
 
 
     @Test
-    public void testCompareReplacementCostLess() {
+    void testCompareReplacementCostLess() {
         testFilm.setReplacementCost(10.52);
         Assertions.assertEquals(0, testFilm.compareReplacementCost(newFilm), "The replacement rate comparison failed on lower");
     }
 
     @Test
-    public void testCompareReplacementCostSame() {
+    void testCompareReplacementCostSame() {
         testFilm.setReplacementCost(10.53);
         Assertions.assertEquals(1, testFilm.compareReplacementCost(newFilm), "The replacement cost comparison failed on same");
     }
 
     @Test
-    public void testCompareReplacementCostMore() {
+    void testCompareReplacementCostMore() {
         testFilm.setReplacementCost(10.54);
         Assertions.assertEquals(2, testFilm.compareReplacementCost(newFilm), "The replacment cost comparison failed on larger");
     }
 
     @Test
-    public void testCompareRatingLess() {
+    void testCompareRatingLess() {
         testFilm.setRating("G");
         Assertions.assertEquals(0, testFilm.compareRating(newFilm), "The rating comparison failed on lower");
     }
 
     @Test
-    public void testCompareRatingSame() {
+    void testCompareRatingSame() {
         testFilm.setRating("PG");
         Assertions.assertEquals(1, testFilm.compareRating(newFilm), "The rating comparison failed on equal");
     }
 
     @Test
-    public void testCompareRatingMore() {
+    void testCompareRatingMore() {
         testFilm.setRating("PG-13");
         Assertions.assertEquals(2, testFilm.compareRating(newFilm), "The rating comparison failed on more");
     }
