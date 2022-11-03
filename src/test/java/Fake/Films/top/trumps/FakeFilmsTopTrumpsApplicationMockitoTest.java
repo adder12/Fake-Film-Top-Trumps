5508242440483f07ade65357247c1c8db97cf9a3
Ryan Garrett(Adder12) <ryan.garrett011@gmail.com>
2022-11-03 16:22:07 +0000

    Possible fix for mockito tests

--- src/test/java/Fake/Films/top/trumps/FakeFilmsTopTrumpsApplicationMockitoTest.java
+++ src/test/java/Fake/Films/top/trumps/FakeFilmsTopTrumpsApplicationMockitoTest.java
@@ -9,29 +9,30 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 
+import java.util.Optional;
+
 import static org.mockito.Mockito.mock;
 
-@ActiveProfiles("test")
-@SpringBootTest(classes = FakeFilmsTopTrumpsApplication.class)
-@RunWith(MockitoJUnitRunner.class)
-class FakeFilmsTopTrumpsApplicationMockitoTest {
+//@ActiveProfiles("test")
+//@SpringBootTest(classes = FakeFilmsTopTrumpsApplication.class)
+//@RunWith(MockitoJUnitRunner.class)
+public class FakeFilmsTopTrumpsApplicationMockitoTest {
 
 
-    @Autowired
     ActorRepository actorRepo = mock(ActorRepository.class);
-    @Autowired
+
     FilmRepository filmRepo = mock(FilmRepository.class);
-    @Autowired
+
     CategoryRepository categoryRepo = mock(CategoryRepository.class);
 
 
-    FakeFilmsTopTrumpsApplication testMain = mock(FakeFilmsTopTrumpsApplication.class);
+    FakeFilmsTopTrumpsApplication testMain = new FakeFilmsTopTrumpsApplication(actorRepo, filmRepo, categoryRepo);
 
     @Test
     public void getSingleActor() {
         Actor test = new Actor("test", "test");
-        Mockito.when(testMain.getSingleActor(1)).thenReturn(test);
+        Mockito.when(actorRepo.findById(1)).thenReturn(Optional.of(test));
         Actor returnedActor = testMain.getSingleActor(1);
-        Assertions.assertEquals(test, test, "The mockito test for Get single actor failed");
+        Assertions.assertEquals(test, returnedActor, "The mockito test for Get single actor failed");
     }
 }
