00f8527afe5dc89da6c7db9ce7817c6f2b01e348
Ryan Garrett(Adder12) <ryan.garrett011@gmail.com>
2022-11-03 17:11:28 +0000

    Added another Mockito test. Category one broken atm, so commented out. on fix list tomorrow

--- src/test/java/Fake/Films/top/trumps/FakeFilmsTopTrumpsApplicationMockitoTest.java
+++ src/test/java/Fake/Films/top/trumps/FakeFilmsTopTrumpsApplicationMockitoTest.java
@@ -2,37 +2,43 @@ package Fake.Films.top.trumps;
 
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.Assertions;
-import org.junit.runner.RunWith;
+
 import org.mockito.Mockito;
-import org.mockito.junit.MockitoJUnitRunner;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ActiveProfiles;
+
 
 import java.util.Optional;
 
 import static org.mockito.Mockito.mock;
 
-//@ActiveProfiles("test")
-//@SpringBootTest(classes = FakeFilmsTopTrumpsApplication.class)
-//@RunWith(MockitoJUnitRunner.class)
+
 public class FakeFilmsTopTrumpsApplicationMockitoTest {
-
-
     ActorRepository actorRepo = mock(ActorRepository.class);
-
     FilmRepository filmRepo = mock(FilmRepository.class);
-
     CategoryRepository categoryRepo = mock(CategoryRepository.class);
-
-
     FakeFilmsTopTrumpsApplication testMain = new FakeFilmsTopTrumpsApplication(actorRepo, filmRepo, categoryRepo);
+    int id = 1;
 
     @Test
     public void getSingleActor() {
-        Actor test = new Actor("test", "test");
-        Mockito.when(actorRepo.findById(1)).thenReturn(Optional.of(test));
-        Actor returnedActor = testMain.getSingleActor(1);
+        Actor test = new Actor();
+        Mockito.when(actorRepo.findById(id)).thenReturn(Optional.of(test));
+        Actor returnedActor = testMain.getSingleActor(id);
         Assertions.assertEquals(test, returnedActor, "The mockito test for Get single actor failed");
     }
+
+    @Test
+    public void getSingleFilm() {
+        Film film = new Film();
+        Mockito.when(filmRepo.findById(id)).thenReturn(Optional.of(film));
+        Film returnedFilm = testMain.getSingleFilm(id);
+        Assertions.assertEquals(film, returnedFilm, "The Mockito test for get Single Film failed");
+    }
+
+//    @Test
+//    public void getSingleCategory() {
+//        Category test = new Category();
+//        Mockito.when(categoryRepo.findById(id)).thenReturn(Optional.of(test));
+//        Category returnedCat = testMain.getSingleCategory(id);
+//        Assertions.assertEquals(test, returnedCat, "The Mockito test for get single category failed");
+//    }
 }
